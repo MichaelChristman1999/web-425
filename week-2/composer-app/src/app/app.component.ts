28c621999a9d41582b8ebc61726824e91c9def6f
Michael Christman <mchristman@my365.bellevue.edu>
2023-08-27 21:48:38 -0500

    Add files via upload

--- week-2/composer-app/src/app/app.component.ts
+++ week-2/composer-app/src/app/app.component.ts
@@ -1,10 +1,23 @@
+/**
+ * Title: app.component.ts
+ * Author: Michael Christman
+ * Date: August 17, 2023
+ * Description: This code defines the main component for the composer app project.
+ * Sources Used:
+ * Angular Tutorial for Beginners video by Professor Krasso
+ * Angular Tutorial - Data Binding & *ng Directives by Professor Krasso
+ * Bootstrap: https://getbootstrap.com/
+ */
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'composer-app';
+  /* Adding Assignment Title */
+  assignment: string = 'Assignment 2.2: Composer App';
 }
--- week-2/composer-app/src/app/composer-list/composer-list.component.ts
+++ week-2/composer-app/src/app/composer-list/composer-list.component.ts
@@ -1,15 +1,46 @@
+/**
+ * Title: composer-list.component.ts
+ * Author: Michael Christman
+ * Date: August 18, 2023
+ * Description: This code defines the composer class for the composer app project.
+ * Sources Used:
+ * Angular Tutorial for Beginners video by Professor Krasso
+ * Angular Tutorial - Data Binding & *ng Directives by Professor Krasso
+ * Bootstrap: https://getbootstrap.com/
+ */
+
 import { Component, OnInit } from '@angular/core';
+
+// Define a class to represent a music composer
+export default class Composer {
+  fullName: string;
+  genre: string;
+
+  constructor(fullName: string, genre: string) {
+    this.fullName = fullName;
+    this.genre = genre;
+  }
+}
 
 @Component({
   selector: 'app-composer-list',
   templateUrl: './composer-list.component.html',
-  styleUrls: ['./composer-list.component.css']
+  styleUrls: ['./composer-list.component.css'],
 })
 export class ComposerListComponent implements OnInit {
+  composers: Array<Composer>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    // Initialize composer instances and populate the array.
+    this.composers = [
+      new Composer('Ludwig van Beethoven', 'Romantic Classical'),
+      new Composer('Wolfgang Amadeus Mozart', ' Classical'),
+      new Composer('John Williams', 'Contemporary Classical'),
+      new Composer('Philip Glass', 'Contemporary Classical'),
+      new Composer('Hans Zimmer', 'Orchestral'),
+      new Composer('George Frideric Handle', 'Baroque'),
+    ];
   }
 
+  ngOnInit(): void {}
 }
