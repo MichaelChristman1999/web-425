d9b63da2d0497d8d4ad6a8d4969203646ca5409e
Michael Christman <mchristman@my365.bellevue.edu>
2023-09-10 19:53:10 -0500

    Add files via upload

---
+++ week-5/in-n-out-booksp1/src/app/book-list/book-list.component.ts
@@ -0,0 +1,20 @@
+/**
+ * Title: book-list.component.ts
+ * Author: Michael Christman
+ * Date: September 9, 2023
+ * Sources Used:
+ * Angular Tutorial for Beginners: Navigation and Layout with Angular Material video by Professor Krasso (https://www.youtube.com/watch?v=Wif3MsDN7Pg)
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.scss'],
+})
+export class BookListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ week-5/in-n-out-booksp1/src/app/contact/contact.component.ts
@@ -0,0 +1,20 @@
+/**
+ * Title: contact.component.ts
+ * Author: Michael Christman
+ * Date: September 9, 2023
+ * Sources Used:
+ * Angular Tutorial for Beginners: Navigation and Layout with Angular Material video by Professor Krasso (https://www.youtube.com/watch?v=Wif3MsDN7Pg)
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss'],
+})
+export class ContactComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
