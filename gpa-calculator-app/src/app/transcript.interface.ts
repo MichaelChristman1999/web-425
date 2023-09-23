52e66bf24da599148262372bd3459746f9032a31
Michael Christman <mchristman@my365.bellevue.edu>
2023-09-23 16:27:01 -0500

    Add files via upload

---
+++ gpa-calculator-app/src/app/transcript.interface.ts
@@ -0,0 +1,11 @@
+/*
+ * Title: transcript.interface.ts
+ * Author: Michael Christman
+ * Date: September 22, 2023
+ * Sources Used: Assignment 6.4 Instructions by Professor Krasso
+ */
+
+export interface ITranscript {
+  course: string;
+  grade: string;
+}
